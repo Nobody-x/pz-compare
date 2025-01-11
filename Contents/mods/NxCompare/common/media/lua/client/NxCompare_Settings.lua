0da0cecabf2f3376b78ed6785272be884a300690
Nobody-x <8191993+Nobody-x@users.noreply.github.com>
2025-01-11 15:25:24 +0100

    Use the new mod options API of build 42

--- Contents/mods/NxCompare/common/media/lua/client/NxCompare_Settings.lua
+++ Contents/mods/NxCompare/common/media/lua/client/NxCompare_Settings.lua
@@ -4,30 +4,35 @@
 NxCompare_IsTooltipShown = false
 
 
--- Key-bind declaration.
-local toggleCompareTooltipsTab = {
-  key = Keyboard.KEY_LSHIFT,
-  name = "toogleCompareTooltips",
+-- Store all configs.
+local config = {
+  toggleCompareTooltips = nil
 }
 
-if ModOptions and ModOptions.AddKeyBinding then
-	-- Adding key-bind to the [UI] menu
-	ModOptions:AddKeyBinding("[UI]", toggleCompareTooltipsTab);
+
+-- Configuration settings build
+local function buildConfig() 
+	local options = PZAPI.ModOptions:create("nxcompare", "Nx's Compare items")
+	
+	config.toggleCompareTooltips = options:addKeyBind("0", getText("UI_options_nxcompare_toogleCompareTooltips"), Keyboard.KEY_LSHIFT)
 end
 
 
 -- Key press observers.
 local function KeyDown(key)
-	if key == toggleCompareTooltipsTab.key then
+	if key == config.toggleCompareTooltips.key then
         NxCompare_IsTooltipShown = true
 	end
 end
 
 local function KeyUp(key)
-	if key == toggleCompareTooltipsTab.key then
+	if key == config.toggleCompareTooltips.key then
         NxCompare_IsTooltipShown = false
 	end
 end
 
+
+-- Handle settings
+buildConfig()
 Events.OnKeyStartPressed.Add(KeyDown)
 Events.OnKeyPressed.Add(KeyUp)
